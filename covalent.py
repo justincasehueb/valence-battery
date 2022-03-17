6ecdc92fbd522d538c70930ac1fd0cf4246bb6ce
Justin <justin.hueb@gmail.com>
2022-03-16 23:13:24 -0500

    String Configuration Tool (final)
    Finalized string-related elements of the GUI, planned paths for iterability among strings and their constituent modules. Expanded configurability to include default text fields for Valence Software

--- covalent.py
+++ covalent.py
@@ -4,6 +4,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from tkinter import *
 from tkinter import messagebox
+import tkinter as tk
 from pywinauto.application import Application
 import time
 import csv
@@ -14,8 +15,39 @@ import glob
 dir = os.path.dirname(__file__)
 os.chdir(dir)
 # print(os.getcwd())
+#app.ModuleDiagG2.print_control_identifiers()
+
 
 configs=None
+def loadConfigs():
+    global configs
+    with open(dir+"\\config.yaml", 'r') as file:
+        configs = yaml.safe_load(file)
+        # print(str(configs['strings'][1]))
+        # print(prime_service['prime_numbers'][0])
+        # print(prime_service['rest']['url'])
+loadConfigs()
+
+##GLOBAL VARIABLES##
+
+root = Tk() #root is the root window
+root.title("Covalent") #snazzy name
+root.geometry(str(configs['resolution']['x'])+"x"+str(configs['resolution']['y'])) #resize root window
+
+headFrame = Frame(root)
+headFrame.pack(fill=BOTH, expand=1)
+
+numStringColumns=configs['num_string_columns']
+columnCounter=0
+
+stringFrame = Frame(root)
+stringFrame.pack(fill=BOTH, expand=1)
+f=Frame(stringFrame)
+f.pack(side=LEFT)
+
+stringList = [] #List of frames that contain information on individual strings
+
+##FUNCTIONS##
 
 def CombineCSV(folder):
     os.chdir(dir+"\\Valence_Logs\\"+str(folder))
@@ -27,16 +59,6 @@ def CombineCSV(folder):
     #export to csv
     combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
 # CombineCSV("")
-
-
-def loadConfigs():
-    global configs
-    with open(dir+"\\config.yaml", 'r') as file:
-        configs = yaml.safe_load(file)
-        # print(str(configs['strings'][1]))
-        # print(prime_service['prime_numbers'][0])
-        # print(prime_service['rest']['url'])
-loadConfigs()
 
 #generates the text in the strings section of the config.YAML file
 def makeYAML():
@@ -53,39 +75,46 @@ def makeFiles():
         if (os.path.isdir(path+str(i))==False):
             os.mkdir(path+str(i))
 
-
-#app.ModuleDiagG2.print_control_identifiers()
-
-root = Tk() #root is the root window
-root.title("Covalent") #snazzy name
-root.geometry(str(configs['resolution']['x'])+"x"+str(configs['resolution']['y'])) #resize root window
-
-e_id = Entry(root)
-e_path = Entry(root,width=50)
-e_com = Entry(root)
-e_id.insert(0,"19")
-e_path.insert(0,"C:\\Users\\user\\Desktop\\Valence\\Valence_Logs")
-e_com.insert(0,"COM6")
-stringFrame = Frame(root)
-
-f=Frame(stringFrame)
-for i in range(configs['num_strings']):
-    if (i%3==0 and i>0):
-        f.pack()
+def AddString():
+    global stringFrame, numStringColumns, columnCounter, f
+    columnCounter=columnCounter+1
+    if (columnCounter==1):
+        f.pack(side='top',fill=X)
+    elif ((columnCounter-1)%numStringColumns==0):
         f=Frame(stringFrame)
-    l=Label(f,text="String #"+str(i+1),pady=50,padx=40).pack(side='left')
-    l1=Listbox(f,height=5,width=10)
-    # print()
-    for i in configs['strings'][i+1]:
-        l1.insert('end',str(i))
-    l1.pack(side='left')
-stringFrame.grid(row=7,column=0,columnspan=10)
+        f.pack(side='top',fill=X)
+        # columnCounter=0
+
+    lbl=Label(f,text="String #"+str(columnCounter),pady=50,padx=40)
+    lbx=Listbox(f,height=5,width=10)
+    lbl.pack(side='left')
+    lbx.pack(side='left')
+    stringList.insert(columnCounter,lbx)
+    PopulateStrings()
+
+def PopulateStrings():
+    global stringList, configs
+    for i in range(1, len(stringList)+1):
+        for j in configs['strings'][i]:
+            if(stringList[i-1].size()<4):
+                stringList[i-1].insert('end',j)
+
+for i in range(configs['num_strings']):
+    # print(i)
+    AddString()
+
+e_id = Entry(headFrame)
+e_path = Entry(headFrame,width=50)
+e_com = Entry(headFrame)
+e_id.insert(0,configs['ID'])
+e_path.insert(0,configs['log_path'])
+e_com.insert(0,configs['COM'])
 
 ## Collects a single sample from the specified module
 # id - integer Module ID number
 # comPort - string with COM port # formatted as "COM#"
 def CollectSample(id,comPort):
-    app=Application(backend='uia').start('C:\Program Files (x86)\Valence Technology Inc\Valence U-Charge XP Module Diagnostics Software\ModuleDiagG2.exe')
+    app=Application(backend='uia').start(configs['path_to_valence'])
 
     #Set COM port
     comPort=app.ModuleDiagG2.child_window(title="COM Port", auto_id="1001", control_type="Edit").wrapper_object()
@@ -135,44 +164,12 @@ def CollectSample(id,comPort):
     btn_CloseWindow = app.ModuleDiagG2.child_window(title="Close", control_type="Button").wrapper_object()
     btn_CloseWindow.click_input()
 
-def readCSV():
-    # global
-    with open('C:/Users/user/Desktop/Valence/Valence_Python/valence-battery/battery.CSV') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-                line_count += 1
-            else:
-                print(f'\ttime:{row[0]}:({row[1]}), SN:{row[2]}, #{row[3]}')
-                tmax=[float(row[4]),float(row[5]),float(row[6]),float(row[7]),float(row[8]),float(row[9])]
-                tmax=np.amax(tmax)
-                print(f'\thighest temp:{tmax}')
-                curr=float(row[11])
+btn_getSample = Button(headFrame,text="Collect a Sample",pady=10,command=lambda: CollectSample(e_id.get(),e_com.get()))
+btn_addString = Button(headFrame,text="Add a String",pady=10,command=AddString)
 
-                vmax=[row[12],row[13],row[14],row[15],row[16],row[17]]
-                i=0
-                for item in vmax:
-                    if (item==""):
-                        vmax[i]=0
-                    else:
-                        vmax[i]=float(item)
-                    i=i+1
-
-                vmax=np.amax(vmax)
-                print(f'\thighest voltage:{vmax}')
-                line_count += 1
-        print(f'Processed {line_count} lines.')
-
-
-btn_getSample = Button(root,text="Collect a Sample",pady=10,command=lambda: CollectSample(e_id.get(),e_com.get()))
-btn_readCSV = Button(root,text="Read CSV",pady=10,command=readCSV)
-
-
-l_id = Label(root,text="Module ID #:")
-l_path = Label(root,text="Log Folder:")
-l_com = Label(root,text="COM port:")
+l_id = Label(headFrame,text="Module ID #:")
+l_path = Label(headFrame,text="Log Folder:")
+l_com = Label(headFrame,text="COM port:")
 l_id.grid(row=1,column=0)
 l_path.grid(row=2,column=0)
 l_com.grid(row=3,column=0)
@@ -182,16 +179,33 @@ e_path.grid(row=2,column=1)
 e_com.grid(row=3,column=1)
 
 btn_getSample.grid(row=5,column=0,columnspan=2)
-btn_readCSV.grid(row=6,column=0,columnspan=2)
+btn_addString.grid(row=6,column=0,columnspan=2)
+
+# stringFrame.grid(row=7,column=0,columnspan=10)
 
 #TODO: add data visualization
-#TODO: add a subfolder creation feature if they don't already exist.
-#TODO: have a subfolder for each module ID's CSV files (..\Valence_Logs\19)
-#TODO: add a config file for critical settings changes
-#TODO: traverse all individual CSVs and compile a master CSV for pandas
 #TODO: implement a "Strings" Customizer so that IDs can be added to ListBoxes and statistics can be done per string
 #TODO: implement a Frame for the Listboxes that can have ListBoxes PACKED in, with buttons for adding/removing strings
 #TODO: having all CSVs save to a single folder and get combined into a master CSV
 
 
 root.mainloop()
+
+# f=Frame(stringFrame)
+# count=0
+# perRow=3
+# def populateStrings():
+#     for i in range(1,configs['num_strings']+1):
+#         print(i)
+#         count=count+1
+#         if (count==perRow):
+#             f.pack()
+#             f=Frame(stringFrame)
+#             count=0
+#         l=Label(f,text="String #"+str(i),pady=50,padx=40).pack(side='left')
+#         l1=Listbox(f,height=5,width=10)
+#         for j in configs['strings'][i]:
+#             l1.insert('end',str(j))
+#         l1.pack(side='left')
+#         if (count>0 and i==configs['num_strings']):
+#             f.pack()
